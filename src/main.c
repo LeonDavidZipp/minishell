894c99ea2c669c84349c50ecd658a56512243c8c
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-12 18:33:07 +0100

    signals working, but control c not yet

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:22:58 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/12 17:18:54 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/12 18:29:57 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,14 @@ int	main(void)
 	while (1)
 	{
 		input = readline("\033[0;36mchl#>	\033[0m");
-		if (input == NULL || ft_strncmp(input, "exit", ft_strlen(input)) == 0)
+		if (input == NULL)
+			return (0);
+		else if (ft_strlen(input) == 0)
+		{
+			free(input);
+			continue ;
+		}
+		else if (ft_strncmp(input, "exit", ft_strlen(input)) == 0)
 		{
 			free(input);
 			return (0);
--- src/signal_handler.c
+++ src/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:58:14 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/12 15:59:54 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/12 18:30:30 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static void		handle_ctrl_c(int signal);
 void	signal_handler(void)
 {
 	signal(SIGINT, handle_ctrl_c);
-	// signal(EOF, handle_ctrl_d);
 	signal(SIGQUIT, SIG_IGN);
 }
 
