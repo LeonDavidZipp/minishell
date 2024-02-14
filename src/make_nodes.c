2d702d64e7eebce11b62c82c8d83860028aa8e10
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-14 15:07:41 +0100

    removed commented out main

--- src/make_nodes.c
+++ src/make_nodes.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 12:48:17 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/14 14:51:20 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/14 15:07:29 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,33 +51,3 @@ void	debug_printtree(t_treenode *root, int tabs)
 		printf("--is empty--\n");
 	}
 }
-
-/*
-int	main(void)
-{
-	t_input		inputs[7];
-	t_treenode	*root;
-	inputs[0].content = "echo";
-	inputs[0].type = BUILTIN_CMD;
-	inputs[1].content = "hello";
-	inputs[1].type = ARG;
-	inputs[2].content = ">";
-	inputs[2].type = REDIR_OUT;
-	inputs[3].content = "file";
-	inputs[3].type = ARG;
-	inputs[4].content = "|";
-	inputs[4].type = PIPE;
-	inputs[5].content = "cat";
-	inputs[5].type = BUILTIN_CMD;
-	inputs[6].content = NULL;
-	inputs[6].type = 0;
-	root = (t_treenode *)new_node(&inputs[0]);
-	root->left = (t_treenode *)new_node(&inputs[1]);
-	root->right = (t_treenode *)new_node(&inputs[2]);
-	root->right->left = (t_treenode *)new_node(&inputs[3]);
-	root->right->right = (t_treenode *)new_node(&inputs[4]);
-	root->right->right->left = (t_treenode *)new_node(&inputs[5]);
-	debug_printtree(root, 0);
-	return (0);
-}
-*/
