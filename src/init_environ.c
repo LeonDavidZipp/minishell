dac9ad19fbca21cc826715da90693d5c1f03ea06
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-15 14:18:55 +0100

    included env var function, added provisoric update functionality

--- src/init_environ.c
+++ src/init_environ.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 20:16:01 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/15 13:25:20 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/15 14:18:23 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_env_var	*init_environ(char **environ)
 		temp = ft_split(*environ, '=');
 		if (!temp)
 			return (free_env_vars(first), NULL);
-		env_var = create_env_var(temp, prev);
+		env_var = new_env_var(temp[0], temp[1], prev);
 		if (!env_var)
 			return (free_env_vars(first), NULL);
 		ft_free_2d_arr((void **)temp);
@@ -41,17 +41,34 @@ t_env_var	*init_environ(char **environ)
 	return (first);
 }
 
+void	add_to_environ(t_env_var *new, t_env_var **env_vars)
+{
+	t_list	*successor;
+	t_list	*current;
 
+	current = *env_vars;
+	if (*env_vars == NULL)
+		*env_vars = new;
+	else
+	{
+		while (current->next != NULL)
+		{
+			successor = current->next;
+			current = successor;
+		}
+		current->next = new;
+	}
+}
 
-t_env_var	*new_env_var(char **temp, t_env_var *prev)
+t_env_var	*new_env_var(char *key, char *value, t_env_var *prev)
 {
 	t_env_var	*env_var;
 
 	env_var = (t_env_var *)malloc(sizeof(t_env_var));
 	if (!env_var)
 		return (NULL);
-	env_var->key = ft_strdup(temp[0]);
-	env_var->value = ft_strdup(temp[1]);
+	env_var->key = ft_strdup(key);
+	env_var->value = ft_strdup(value);
 	if (!env_var->key || !env_var->value)
 	{
 		free(env_var->key);
@@ -63,6 +80,12 @@ t_env_var	*new_env_var(char **temp, t_env_var *prev)
 	if (prev)
 		prev->next = env_var;
 	return (env_var);
+}
+
+void	update_env_var(t_env_var *env_var, char *value)
+{
+	free(env_var->value);
+	env_var->value = ft_strdup(value);
 }
 
 static void	free_env_vars(t_env_var *env_var)
@@ -77,4 +100,18 @@ static void	free_env_vars(t_env_var *env_var)
 		free(env_var);
 		env_var = temp;
 	}
+}
+
+int	main(int argc, char **argv, char **environ)
+{
+	t_env_var	*env_var;
+
+	(void)argc;
+	(void)argv;
+	env_var = init_environ(environ);
+	if (!env_var)
+		return (1);
+	t_env_var	*temp = new_env_var("test", "test", env_var);
+	free_env_vars(env_var);
+	return (0);
 }
