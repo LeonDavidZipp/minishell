12a3c7f4793621ca34b7815b7ba141d7659fc4d5
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-16 18:04:04 +0100

    added split_environ

--- src/env_vars/init_environ.c
+++ src/env_vars/init_environ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_environ.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:04:21 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/16 15:11:25 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/16 18:03:24 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ t_env_var	*init_environ(char **environ)
 	prev = NULL;
 	while (*environ)
 	{
-		temp = ft_split(*environ, '=');
+		// temp = ft_split(*environ, '=');
+		temp = split_environ(environ);
 		if (!temp)
 			return (free_env_vars(first), NULL);
 		if (ft_null_terminated_arr_len((void **)temp) != 2)
@@ -50,4 +51,25 @@ static void	update_links(t_env_var **first, t_env_var **prev,
 	else
 		(*prev)->next = env_var;
 	*prev = env_var;
+}
+
+char	**split_environ(char *environ)
+{
+	char		*key;
+	char		*value;
+	char		**key_value;
+	int i;
+	int			len;
+
+	key_value = ft_calloc(2, sizeof(char *));
+	if (!key_value)
+		return (NULL);
+	len = 0;
+	while (environ[len] != '=')
+	{
+		len++;
+	}
+	key_value[0] = ft_substr(environ, 0, len);
+	key_value[1] = ft_substr(environ, len, ft_strlen(environ) - len);
+	return (key_value);
 }
