65c84dd2d5c9a71659a2b8a9dbdfc2f84a51b734
cgerlinger <gerlinger.68@web.de>
2024-02-26 15:03:10 +0100

    fixed leak and added safety checks in split

--- src/parsing/split.c
+++ src/parsing/split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: intra <intra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cgerling <cgerling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 11:07:56 by cgerling          #+#    #+#             */
-/*   Updated: 2024/02/25 19:57:21 by intra            ###   ########.fr       */
+/*   Updated: 2024/02/26 15:00:47 by cgerling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,8 @@ int	count_tokens(char *input)
 	{
 		quotes_brackets(input[i], &flags[0], &flags[1], &flags[2]);
 		if ((!flags[0] && !flags[1] && !flags[2] && !is_space(input[i]))
-			&& (is_space(input[i - 1]) || input[i - 1] == '\''
-				|| input[i - 1] == '"' || input[i - 1] == '(' || was_flag))
+			&& (i == 0 || (is_space(input[i - 1]) || input[i - 1] == '\''
+					|| input[i - 1] == '"' || input[i - 1] == '(' || was_flag)))
 		{
 			amount++;
 		}
@@ -53,11 +53,11 @@ int	count_tokens(char *input)
 int	condition(char *input, int *count, bool *flags, bool was_flag)
 {
 	if ((!flags[0] && !flags[1] && !flags[2] && !is_space(input[count[1]]))
-		&& (is_space(input[count[1] - 1]) || input[count[1] - 1] == '\''
-			|| input[count[1] - 1] == '"' || input[count[1] - 1] == '('
-			|| was_flag || (ft_isprint(input[count[1] - 1])
-				&& (input[count[1]] == '\'' || input[count[1]] == '"'
-					|| input[count[1]] == '('))))
+		&& (count[1] > 0 || (is_space(input[count[1] - 1])
+				|| input[count[1] - 1] == '\'' || input[count[1] - 1] == '"'
+				|| input[count[1] - 1] == '(' || was_flag
+				|| (ft_isprint(input[count[1] - 1]) && (input[count[1]] == '\''
+						|| input[count[1]] == '"' || input[count[1]] == '(')))))
 	{
 		return (1);
 	}
@@ -81,13 +81,11 @@ int	process_token(char *input, int *count, bool *flags, char **tokens)
 		if (condition(input, count, flags, was_flag))
 			break ;
 	}
-	tokens[count[2]] = ft_trim_in_place(
-			ft_substr(input, count[0], count[1] - count[0]), " \t");
+	if (count[1] > 0 && input[count[1] - 1] == ' ')
+		count[1]--;
+	tokens[count[2]] = ft_substr(input, count[0], count[1] - count[0]);
 	if (!tokens[count[2]])
-	{
-		ft_free_2d_arr((void **)tokens);
-		return (free(input), 0);
-	}
+		return (ft_free_2d_arr((void **)tokens), free(input), 0);
 	count[0] = count[1];
 	count[2]++;
 	return (1);
@@ -118,18 +116,18 @@ char	**split(char *input)
 	return (tokens);
 }
 
-// int main()
-// {
-// 	char **str = split("'\"'\"'\"hello\"'\"'\"' test hallo echo hi&&echo hi");
-// 	// char **str = split("echo hi was");
-// 	for (int i = 0; str[i]; i++)
-// 	{
-// 		printf("%s\n", str[i]);
-// 	}
-// 	for (int i = 0; str[i]; i++)
-// 	{
-// 		free(str[i]);
-// 	}
-// 	free(str);
-// 	return 0;
-// }
+int main()
+{
+	char **str = split("'\"'\"'\"hello\"'\"'\"' test hallo echo hi&&echo hi");
+	// char **str = split("echo hi was");
+	for (int i = 0; str[i]; i++)
+	{
+		printf("%s\n", str[i]);
+	}
+	for (int i = 0; str[i]; i++)
+	{
+		free(str[i]);
+	}
+	free(str);
+	return 0;
+}
