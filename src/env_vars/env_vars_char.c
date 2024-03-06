9638262188885a3c07307b0b74f3c8684db93156
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-06 19:26:24 +0100

    nothing special

--- src/env_vars/env_vars_char.c
+++ src/env_vars/env_vars_char.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:19:56 by lzipp             #+#    #+#             */
-/*   Updated: 2024/03/06 18:43:20 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/06 19:21:57 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	**update_env_vars(char *key, char *value, char **env_vars)
 			sizeof(char *));
 	if (!env_vars || !env_vars)
 		return (NULL);
-	env_vars[i + 1] = ft_strjoin(new_var, value);
+	env_vars[i] = ft_strjoin(new_var, value);
 	free(new_var);
 	return (env_vars);
 }
@@ -49,13 +49,14 @@ char	**unset_env_var(char *key, char **env_vars)
 	{
 		if (ft_strncmp(env_vars[i], key, ft_strlen(key)) == 0)
 		{
-			free(env_vars[i]);
+			printf("found match: %s\n", env_vars[i]);
 			while (env_vars[i])
 			{
+				free(env_vars[i]);
 				env_vars[i] = env_vars[i + 1];
 				i++;
 			}
-			return (env_vars);
+			break ;
 		}
 	}
 	return (env_vars);
@@ -66,6 +67,7 @@ char	**init_envp(char **env_vars)
 	int			i;
 	char		**envp;
 
+	i = -1;
 	envp = ft_calloc(ft_null_terminated_arr_len((void **)env_vars) + 1,
 			sizeof(char *));
 	if (!envp)
@@ -81,11 +83,17 @@ int	main(int argc, char **argv, char **environ)
 
 	(void)argc;
 	(void)argv;
-	env_vars = environ;
+	env_vars = init_envp(environ);
 	env_vars = update_env_vars("TEST", "test", env_vars);
 	env_vars = update_env_vars("TEST2", "test2", env_vars);
 	env_vars = update_env_vars("TEST3", "test3", env_vars);
 	int i = -1;
+	while (env_vars[++i])
+	{
+		printf("%s\n", env_vars[i]);
+	}
+	env_vars = unset_env_var("TEST2", env_vars);
+	i = -1;
 	while (env_vars[++i])
 	{
 		printf("%s\n", env_vars[i]);
