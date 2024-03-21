8e0eb1e8e249183ff029d05cd5201080e83ab70e
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-21 10:48:14 +0100

    prevented segfaults happening in tokens_valid, removed cd checker

--- src/lexer/check_tokens_valid.c
+++ src/lexer/check_tokens_valid.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 22:01:18 by lzipp             #+#    #+#             */
-/*   Updated: 2024/03/20 15:28:58 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/21 10:47:27 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 static bool	check_and_or_pipe_redir_out_append(t_token *current);
 static bool	check_redir_in_heredoc(t_token *current);
-static bool	check_cd(t_token *current);
 static bool	check_echo(t_token *current);
 
 bool	check_tokens_valid(t_token *tokens)
 {
 	t_token		*current;
 
+	if (!tokens)
+		return (true);
 	if (tokens->type == OR || tokens->type == AND || tokens->type == PIPE)
 	{
 		if (tokens->next)
@@ -36,8 +37,6 @@ bool	check_tokens_valid(t_token *tokens)
 			return (false);
 		if (!check_redir_in_heredoc(current))
 			return (false);
-		if (!check_cd(current))
-			return (false);
 		if (!check_echo(current))
 			return (false);
 		current = current->next;
@@ -47,6 +46,8 @@ bool	check_tokens_valid(t_token *tokens)
 
 static bool	check_and_or_pipe_redir_out_append(t_token *current)
 {
+	if (!current)
+		return (true);
 	if (current->type == AND || current->type == OR || current->type == PIPE)
 	{
 		if (!current->next)
@@ -73,6 +74,8 @@ static bool	check_and_or_pipe_redir_out_append(t_token *current)
 
 static bool	check_redir_in_heredoc(t_token *current)
 {
+	if (!current)
+		return (true);
 	if (current->type == REDIR_IN || current->type == HEREDOC)
 	{
 		if (!current->next)
@@ -89,34 +92,13 @@ static bool	check_redir_in_heredoc(t_token *current)
 	return (true);
 }
 
-static bool	check_cd(t_token *current)
-{
-	if (ft_strcmp(current->content, "cd") == 0)
-	{
-		if (current && current->next && current->next->next
-			&& current->next->type != AND && current->next->type != OR
-			&& current->next->type != PIPE && current->next->type != REDIR_OUT
-			&& current->next->type != REDIR_APPEND
-			&& current->next->type != REDIR_IN && current->next->type != HEREDOC
-			&& current->next->next->type != AND
-			&& current->next->next->type != OR
-			&& current->next->next->type != PIPE
-			&& current->next->next->type != REDIR_OUT
-			&& current->next->next->type != REDIR_APPEND
-			&& current->next->next->type != REDIR_IN
-			&& current->next->next->type != HEREDOC)
-			return (printf("%s: cd: %s: too many arguments\n", current->next->content,
-					NAME), false);
-		
-	}
-	return (true);
-}
-
 static bool	check_echo(t_token *current)
 {
+	if (!current)
+		return (true);
 	if (ft_strcmp(current->content, "echo") == 0)
 	{
-		if (current && current->next && current->next->next
+		if (current->next && current->next->next
 			&& (current->next->type == LEFT_BRACKET
 				|| current->next->type == RIGHT_BRACKET))
 		{
