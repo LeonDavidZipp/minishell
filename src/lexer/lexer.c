e5894da62e92965a5fab1d889a96afdbcbd68edb
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-26 17:49:57 +0100

    made lexer norm conform

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,14 +6,13 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:52:11 by lzipp             #+#    #+#             */
-/*   Updated: 2024/03/26 17:11:55 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/26 17:49:12 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static void	free_for_next_call(t_app_data *app_data);
-static void	handle_error(t_app_data *app_data);
+static void	free_for_next_call(t_app_data *app_data, t_treenode *ast);
 
 void	lexer(t_app_data *app_data)
 {
@@ -21,50 +20,32 @@ void	lexer(t_app_data *app_data)
 	t_treenode	*intermediate_tree;
 	t_treenode	*ast;
 
-	// 1. first validate input (according to braces, quotes, etc.)
 	if (check_input(app_data->input) == 1)
 	{
-		free_for_next_call(app_data);
+		free_for_next_call(app_data, NULL);
 		return ;
 	}
-	// 2. then tokenize & 3. expand where needed
-	tokens = tokenize(app_data);
-	if (!tokens)
-		handle_error(app_data);
-	// 4. check if the tree is valid
-	if(!check_tokens_valid(tokens))
+	tokens = tokenize(app_data->input);
+	if (!tokens || !check_tokens_valid(tokens))
 	{
-		free_for_next_call(app_data);
+		free_for_next_call(app_data, NULL);
 		return ;
 	}
-	intermediate_tree = combine_cmds_args(tokens);
-	intermediate_tree = switch_heredocs(intermediate_tree);
+	intermediate_tree = switch_heredocs(combine_cmds_args(tokens));
 	free_tokens(tokens);
 	if (!intermediate_tree)
-		handle_error(app_data);
-	// 5. to binary tree
-	ast = NULL;
-	ast = build_ast(ast, intermediate_tree, 0);
+		free_for_next_call(app_data, NULL);
+	ast = build_ast(NULL, intermediate_tree, 0);
 	free_treenodes(intermediate_tree);
-	if (!ast)
-		handle_error(app_data);
-	debug_printtree(ast, 0);
-	// 6. execute
-	execute(app_data, ast);
+	if (ast)
+		execute(app_data, ast);
 	g_exit_signal = 0;
-	// 7. free memory
-	free_treenodes(ast);
-	free_for_next_call(app_data);
+	free_for_next_call(app_data, ast);
 }
 
-static void	free_for_next_call(t_app_data *app_data)
+static void	free_for_next_call(t_app_data *app_data, t_treenode *ast)
 {
 	free(app_data->input);
 	app_data->input = NULL;
-}
-
-static void	handle_error(t_app_data *app_data)
-{
-	printf("%s", LEXER_ERR);
-	free_for_next_call(app_data);
+	free_treenodes(ast);
 }
