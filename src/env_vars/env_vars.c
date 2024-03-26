c8956616347319af29ebb9e5169932baed3896b1
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-26 18:20:35 +0100

    fixed export again

--- src/env_vars/env_vars.c
+++ src/env_vars/env_vars.c
@@ -5,42 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/25 17:25:17 by lzipp             #+#    #+#             */
-/*   Updated: 2024/03/26 14:16:42 by lzipp            ###   ########.fr       */
+/*   Created: 2024/03/26 18:14:00 by lzipp             #+#    #+#             */
+/*   Updated: 2024/03/26 18:20:21 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 static bool	var_name_valid(char *key);
-static bool	update_existing_env_var(char *key, char *value, bool inc_equal,
+static bool	update_existing_env_var(char *key, char *value, int inc_equal,
 				char ***env_vars_ptr);
-
-// char	**update_env_vars(char *key, char *value, bool inc_equal,
-// 				char **env_vars)
-// {
-// 	char	*new_var;
-// 	int		len;
-
-// 	if (update_existing_env_var(key, value, inc_equal, &env_vars))
-// 		return (env_vars);
-// 	len = ft_null_terminated_arr_len((void **)env_vars);
-// 	if (var_name_valid(key))
-// 	{
-// 		if (inc_equal)
-// 			new_var = ft_strjoin(key, "=");
-// 		else
-// 			new_var = ft_strdup(key);
-// 		env_vars = ft_recalloc(env_vars, len + 2, sizeof(char *));
-// 		if (!env_vars)
-// 			return (NULL);
-// 		env_vars[len] = ft_strjoin(new_var, value);
-// 		free(new_var);
-// 	}
-// 	else
-// 		ft_fprintf(2, "%s: export: `%s': not a valid identifier\n", NAME, key);
-// 	return (env_vars);
-// }
 
 char	**update_env_vars(t_envvar **var, int *exit_code, char **env_vars)
 {
@@ -110,32 +84,28 @@ int	unset_env_vars(char *keys_string, char ***env_vars)
 	return (0);
 }
 
-static bool	update_existing_env_var(char *key, char *value, bool inc_equal,
-				char ***env_vars)
+static bool	update_existing_env_var(char *key, char *value, int inc_equal,
+				char ***env_vars_ptr)
 {
 	int			i;
 	char		*new_var;
+	char		**env_vars;
 
-	i = 0;
-	while (env_vars[i])
+	env_vars = *env_vars_ptr;
+	i = -1;
+	while (env_vars[++i])
 	{
-		// ft_printf("key: %s %d\n", key, i);
-		if (ft_strncmp((*env_vars)[i], key, ft_strlen(key)) == 0 && inc_equal)
+		if (ft_strncmp(env_vars[i], key, ft_strlen(key)) == 0 && inc_equal)
 		{
-			
-			printf("triggered 1\n");
-			free((*env_vars)[i]);
+			free(env_vars[i]);
 			new_var = ft_strjoin(key, "=");
-			(*env_vars)[i] = ft_strjoin(new_var, value);
+			env_vars[i] = ft_strjoin(new_var, value);
 			free(new_var);
 			return (true);
 		}
-		else if (ft_strncmp((*env_vars)[i], key, ft_strlen(key)) == 0 && !inc_equal)
-		{
-			printf("triggered 2\n");
+		else if (ft_strncmp(env_vars[i], key, ft_strlen(key)) == 0
+			&& !inc_equal)
 			return (true);
-		}
-		i++;
 	}
 	return (false);
 }
