45ba3c55c01cfe87d4f29589ef6fda2868682cd3
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-06 16:37:58 +0200

    fixed cd -

--- src/builtin_cmds/cd.c
+++ src/builtin_cmds/cd.c
@@ -6,20 +6,19 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 15:56:02 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/06 12:53:30 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/06 14:37:16 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 // implement cd ~ and cd -
-static int	handle_minus(char ***env_vars);
+static int	handle_minus(char ***env_vars, int *last_exit_code);
+static int	update_pwds(char ***env_vars, int *last_exit_code);
 
 int	builtin_cd(char *path, char ***env_vars, int *last_exit_code)
 {
-	t_envvar	*new_var;
-	char		*temp;
-
+	(void)last_exit_code;
 	if (!path)
 	{
 		path = getenv("HOME");
@@ -30,21 +29,17 @@ int	builtin_cd(char *path, char ***env_vars, int *last_exit_code)
 		}
 	}
 	if (path[0] == '-' && ft_strlen(path) == 1)
-		return (handle_minus(env_vars));
+		return (handle_minus(env_vars, last_exit_code));
 	if (chdir(path) == -1)
 	{
 		ft_fprintf(2, "%s: cd: %s: %s\n", NAME, path, strerror(errno));
 		return (1);
 	}
-	temp = ft_sprintf("PWD=%s", getcwd(NULL, 0));
-	new_var = split_env_var(temp);
-	free(temp);
-	*env_vars = update_env_vars(&new_var, last_exit_code, *env_vars);
-	return (0);
+	return (update_pwds(env_vars, last_exit_code));
 }
 
 
-static int	handle_minus(char ***env_vars)
+static int	handle_minus(char ***env_vars, int *last_exit_code)
 {
 	char	*oldpwd;
 	char	*pwd;
@@ -66,5 +61,30 @@ static int	handle_minus(char ***env_vars)
 		ft_fprintf(2, "%s: cd: %s: %s\n", NAME, oldpwd, strerror(errno));
 		return (1);
 	}
+	update_pwds(env_vars, last_exit_code);
 	return (0);
+}
+
+static int	update_pwds(char ***env_vars, int *last_exit_code)
+{
+	char		*oldpwd_string;
+	char		*pwd_string;
+	char		*cwd_string;
+	t_envvar	*new_pwd;
+	t_envvar	*new_oldpwd;
+
+	cwd_string = getcwd(NULL, 0);
+	pwd_string = ft_getenv("PWD", *env_vars);
+	oldpwd_string = ft_strdup("OLDPWD=");
+	oldpwd_string = ft_join_in_place(oldpwd_string, pwd_string);
+	free(pwd_string);
+	pwd_string = ft_sprintf("PWD=%s", cwd_string);
+	free(cwd_string);
+	new_pwd = split_env_var(pwd_string);
+	free(pwd_string);
+	new_oldpwd = split_env_var(oldpwd_string);
+	free(oldpwd_string);
+	*env_vars = update_env_vars(&new_pwd, last_exit_code, *env_vars);
+	*env_vars = update_env_vars(&new_oldpwd, last_exit_code, *env_vars);
+	return (*last_exit_code != 0);
 }
