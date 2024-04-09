dcbd01988651fde79e15ccc11d31a21a243edaa8
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-09 12:16:13 +0200

    nothing special

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:52:11 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/09 12:09:25 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/09 12:15:19 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,8 @@ int	lexer(t_app_data *app_data)
 		return (free_for_next_call(app_data, NULL), 1);
 	}
 	tokens = tokenize(app_data->input, &tmp);
-	// printf("tmp after tokenize: %d\n", tmp);
 	if (!tokens)
 		return (free_for_next_call(app_data, NULL), 1);
-	// tmp = check_tokens_valid(tokens);
 	if (tmp != 0)
 	{
 		app_data->last_exit_code = tmp;
