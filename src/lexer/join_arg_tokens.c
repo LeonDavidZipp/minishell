7a7ed51799edcbba55b36abe316edf1540cde443
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-11 16:45:02 +0200

    removed unnecessary functions & files regarding token joining

--- src/lexer/join_arg_tokens.c
+++ src/lexer/join_arg_tokens.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   join_args_and_after_echo.c                         :+:      :+:    :+:   */
+/*   join_args_tokens.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 11:00:18 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/08 18:56:25 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/11 16:41:44 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,6 @@
 
 static void	join_tokens_helper(t_token **join, t_token **prev,
 				t_token **tokens);
-// static void	join_after_echo_loop(int *echo_flag, t_token **tokens,
-// 				t_token **join, t_token **prev);
-
-// t_token	*join_arg_tokens(t_token *tokens)
-// {
-// 	t_token		*first;
-// 	t_token		*prev;
-// 	t_token		*join;
-
-// 	first = tokens;
-// 	prev = NULL;
-// 	join = NULL;
-// 	while (tokens)
-// 	{
-// 		if (tokens->type == ARG)
-// 			join_tokens_helper(&join, &prev, &tokens);
-// 		else if (tokens->type != ARG)
-// 		{
-// 			join = NULL;
-// 			prev = tokens;
-// 			prev->next = tokens->next;
-// 			tokens = tokens->next;
-// 		}
-// 		if (first == tokens && tokens->type == ARG)
-// 			first = join;
-// 	}
-// 	return (first);
-// }
 
 t_token	*join_arg_tokens(t_token *tokens)
 {
@@ -83,51 +55,6 @@ t_token	*join_arg_tokens(t_token *tokens)
 	}
 	return (first);
 }
-
-// t_token	*join_after_echo(t_token *tokens)
-// {
-// 	t_token		*first;
-// 	t_token		*prev;
-// 	t_token		*join;
-// 	int			echo_flag;
-
-// 	first = tokens;
-// 	prev = NULL;
-// 	join = NULL;
-// 	echo_flag = 0;
-// 	join_after_echo_loop(&echo_flag, &tokens, &join, &prev);
-// 	return (first);
-// }
-
-// static void	join_after_echo_loop(int *echo_flag, t_token **tokens,
-// 				t_token **join, t_token **prev)
-// {
-// 	while (*tokens)
-// 	{
-// 		if (*echo_flag == 0 && ft_strcmp((*tokens)->content, "echo") == 0)
-// 			*echo_flag = 1;
-// 		else if (*echo_flag == 1)
-// 		{
-// 			if ((*tokens)->type == REDIR_APPEND || (*tokens)->type == AND
-// 				|| (*tokens)->type == OR || (*tokens)->type == REDIR_OUT
-// 				|| (*tokens)->type == REDIR_IN || (*tokens)->type == PIPE
-// 				|| (*tokens)->type == HEREDOC || (*tokens)->type == LEFT_BRACKET
-// 				|| (*tokens)->type == RIGHT_BRACKET)
-// 				*echo_flag = 0;
-// 			else
-// 			{
-// 				join_tokens_helper(join, prev, tokens);
-// 				continue ;
-// 			}
-// 		}
-// 		else
-// 			*echo_flag = 0;
-// 		*join = NULL;
-// 		*prev = *tokens;
-// 		(*prev)->next = (*tokens)->next;
-// 		*tokens = (*tokens)->next;
-// 	}
-// }
 
 static void	join_tokens_helper(t_token **join, t_token **prev,
 	t_token **tokens)
