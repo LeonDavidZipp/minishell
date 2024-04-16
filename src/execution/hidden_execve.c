6ea871758926f510800b0fdc057ff043c2e4af7e
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-16 18:42:50 +0200

    first adjustments to hidden_execve.c:  reassign_arg_arr

--- src/execution/hidden_execve.c
+++ src/execution/hidden_execve.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 18:02:08 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/16 18:25:35 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/16 18:42:23 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,38 +22,47 @@ int		hidden_execve(char *hidden_command, t_app_data *app,
 				t_pid_list **pid_list);
 int		add_to_pid_list(pid_t pid, t_pid_list **pidlist);
 
+static int	reassign_arg_arr(char ***arg_arr, t_app_data **app)
+{
+	int			i;
+	char		*tmp;
+
+	i = 0;
+	while ((*arg_arr)[i])
+	{
+		tmp = expand_and_remove((*arg_arr)[i], (*app)->last_exit_code,
+			(*app)->env_vars, 0);
+		if (!tmp)
+			return (1);
+		free((*arg_arr)[i]);
+		(*arg_arr)[i] = tmp;
+		i++;
+	}
+	return (0);
+}
+
 int	hidden_execve(char *hidden_command, t_app_data *app, t_pid_list **pid_list)
 {
 	pid_t	pid;
 	char	*tmp;
 	char	**arg_arr;
-	int		i;
 	int 	flags[2];
 	bool	flag = false;
 
 	flags[0] = 0;
 	flags[1] = 1;
 	tmp = expand(hidden_command, app->last_exit_code,
-		app->env_vars, flags); // NULL checks
+		app->env_vars, flags);
+	if (!tmp)
+		return (1);
 	arg_arr = split(tmp);
 	free(tmp);
-	i = 0;
-	while (arg_arr[i])
-	{
-		tmp = expand_and_remove(arg_arr[i], app->last_exit_code,
-			app->env_vars, 0);
-		if (!tmp)
-			return (ft_free_2d_arr((void **)arg_arr), 1);
-		free(arg_arr[i]);
-		arg_arr[i] = tmp;
-		i++;
-	}
+	if (reassign_arg_arr(&arg_arr, &app) == 1)
+		return (ft_free_2d_arr((void **)arg_arr), 1);
 	pid = fork();
 	if (pid == -1)
-	{
-		ft_fprintf(2, "%s: fork error: %s\n", NAME, strerror(errno));
-		return 1;
-	}
+		return (ft_fprintf(2, "%s: fork error: %s\n", NAME,
+				strerror(errno)), 1);
 	if (pid == 0)
 	{
 		struct stat path_stat;
