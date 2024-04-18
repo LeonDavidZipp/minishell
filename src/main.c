a11a9b0461e44756617799634420603a0cc2f053
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-18 11:54:27 +0200

    fixed printing and test cases

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:24:57 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/16 11:53:24 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/18 11:53:38 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 static void	init_app_data(t_app_data *app_data, char **envp);
 static void	get_input(t_app_data *app_data);
-// static void	print_logo(void);
+static char	*read_input(void);
+static void	print_logo(void);
 
 int	g_exit_signal = 0;
 
@@ -25,12 +26,13 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	if (argc != 1)
 	{
-		// ft_fprintf(2, "\033[0;31mUsage: ./minishell\033[0m\n");
+		ft_fprintf(2, "\033[0;31mUsage: ./minishell\033[0m\n");
 		return (1);
 	}
 	init_app_data(&app_data, envp);
 	signal_handler();
-	// print_logo();
+	if (!app_data.noninteractive)
+		print_logo();
 	while (true)
 	{
 		g_exit_signal = 0;
@@ -44,25 +46,30 @@ int	main(int argc, char **argv, char **envp)
 	return (app_data.last_exit_code);
 }
 
-static void	get_input(t_app_data *app_data)
+static char	*read_input(void)
 {
-	char	*input;
+	char		*input;
+	char		*line;
 
-	
-	// remove this after testing
 	if (isatty(fileno(stdin)))
 		input = readline(PROMPT);
 	else
 	{
-		char *line;
 		line = get_next_line(fileno(stdin));
 		if (line == NULL)
-			return ;
+			return (NULL);
 		input = ft_strtrim(line, "\n");
 		free(line);
 	}
-	// end of remove
-	// input = readline(PROMPT);
+	return (input);
+}
+static void get_input(t_app_data *app_data)
+{
+	char		*input;
+
+	input = read_input();
+	if (!input)
+		return ;
 	if (input && ft_strlen(input) == 0)
 	{
 		free(input);
@@ -81,6 +88,7 @@ static void	get_input(t_app_data *app_data)
 	add_history(input);
 	app_data->input = input;
 }
+
 static void	init_app_data(t_app_data *app_data, char **envp)
 {
 	app_data->env_vars = init_envp(envp);
@@ -89,24 +97,24 @@ static void	init_app_data(t_app_data *app_data, char **envp)
 	app_data->noninteractive = !isatty(fileno(stdin));
 }
 
-// static void	print_logo(void)
-// {
-// 	char	*lzipp;
-// 	char	*cgerling;
+static void	print_logo(void)
+{
+	char	*lzipp;
+	char	*cgerling;
 
-// 	lzipp = "https://profile.intra.42.fr/users/lzipp";
-// 	cgerling = "https://profile.intra.42.fr/users/cgerling";
-// 	printf("\033[0;32m\n\n");
-// 	printf("	██████╗  █████╗ ██████╗  █████╗ ███████╗██╗  ██╗\n");
-// 	printf("	██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██║  ██║\n");
-// 	printf("	██████╔╝███████║██████╔╝███████║███████╗███████║\n");
-// 	printf("	██╔══██╗██╔══██║██╔══██╗██╔══██║╚════██║██╔══██║\n");
-// 	printf("	██████╔╝██║  ██║██████╔╝██║  ██║███████║██║  ██║\n");
-// 	printf("	╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝");
-// 	printf("\033[1;33m\n\n");
-// 	printf("		   born again born again shell\n\n");
-// 	printf("\033[1;36m			");
-// 	printf("\e]8;;%s\alzipp\e]8;;\a && ", lzipp);
-// 	printf("\e]8;;%s\acgerling\e]8;;\a\n", cgerling);
-// 	printf("\033[0m\n");
-// }
+	lzipp = "https://profile.intra.42.fr/users/lzipp";
+	cgerling = "https://profile.intra.42.fr/users/cgerling";
+	printf("\033[0;32m\n\n");
+	printf("	██████╗  █████╗ ██████╗  █████╗ ███████╗██╗  ██╗\n");
+	printf("	██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██║  ██║\n");
+	printf("	██████╔╝███████║██████╔╝███████║███████╗███████║\n");
+	printf("	██╔══██╗██╔══██║██╔══██╗██╔══██║╚════██║██╔══██║\n");
+	printf("	██████╔╝██║  ██║██████╔╝██║  ██║███████║██║  ██║\n");
+	printf("	╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝");
+	printf("\033[1;33m\n\n");
+	printf("		   born again born again shell\n\n");
+	printf("\033[1;36m			");
+	printf("\e]8;;%s\alzipp\e]8;;\a && ", lzipp);
+	printf("\e]8;;%s\acgerling\e]8;;\a\n", cgerling);
+	printf("\033[0m\n");
+}
